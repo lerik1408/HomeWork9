af84dee5c492da06fd4d2abe79ad575b86a0f4f7
ilya <popruzhuk.38@gmail.com>
2019-07-18 14:11:55 +0300

    fix accInfo add fix lint

--- app/router.js
+++ app/router.js
@@ -1,20 +1,20 @@
 const Router = require('koa-router');
-const ctrl = require('./controllers')
+const ctrl = require('./controllers');
 
-const router =new Router();
+const router = new Router();
 
-router.get('index',ctrl.homePage);
-router.get('',ctrl.homePage);
-router.get('siginIn',ctrl.siginInPage);
-router.get('passwordRecovery',ctrl.passwordRecoveryPage)
-router.get('passwordRecovery1',ctrl.passwordRecovery1Page)
-router.get('passwordRecovery2',ctrl.passwordRecovery2Page)
-router.get('siginUp',ctrl.siginUpPage);
-router.get('siginUp1',ctrl.siginUp1Page);
-router.get('siginUp2',ctrl.siginUp2Page);
-router.get('accInfo',ctrl.accInfoPage);
-router.get('personInfo',ctrl.personInfoPage);
-router.get('search',ctrl.searchPage);
-router.get('messenger',ctrl.messengerPage);
+router.get('index', ctrl.homePage);
+router.get('', ctrl.homePage);
+router.get('siginIn', ctrl.siginInPage);
+router.get('passwordRecovery', ctrl.passwordRecoveryPage);
+router.get('passwordRecovery1', ctrl.passwordRecovery1Page);
+router.get('passwordRecovery2', ctrl.passwordRecovery2Page);
+router.get('siginUp', ctrl.siginUpPage);
+router.get('siginUp1', ctrl.siginUp1Page);
+router.get('siginUp2', ctrl.siginUp2Page);
+router.get('accInfo', ctrl.accInfoPage);
+router.get('personInfo', ctrl.personInfoPage);
+router.get('search', ctrl.searchPage);
+router.get('messenger', ctrl.messengerPage);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,34 +3,25 @@ const path = require('path');
 const views = require('koa-views');
 const Router = require('koa-router');
 const serve = require('koa-static');
-const sass = require('node-sass');
+// const sass = require('node-sass');
 
 const app = new Koa();
-const router =new Router();
+const router = new Router();
 
-app.use(views(path.join(__dirname, '/app/template'),{
-    extension :'njk',
-    map: {
-      njk: 'nunjucks'
-    }
-  }));
+app.use(views(path.join(__dirname, '/app/template'), {
+  extension: 'njk',
+  map: {
+    njk: 'nunjucks',
+  },
+}));
 
-app.use(serve(path.join(__dirname, "/app/public")));
+app.use(serve(path.join(__dirname, '/app/public')));
 router.use('/', require('./app/router').routes());
 
 
 app.use(router.routes());
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
-
-
-
-
-
-
-
-
-
 
 
 // app.use(async (ctx) => {
